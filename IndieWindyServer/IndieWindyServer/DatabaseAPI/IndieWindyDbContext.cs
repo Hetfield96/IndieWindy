bcb711ac755a373fb3472a71266b7456d9ae1283
Sergey Roytman <SRoytman@croc.ru>
2020-03-30 16:56:14 +0300

    [Back & Front] Change connection string with azure DB, change gradle min sdk

--- IndieWindyServer/IndieWindyServer/DatabaseAPI/IndieWindyDbContext.cs
+++ IndieWindyServer/IndieWindyServer/DatabaseAPI/IndieWindyDbContext.cs
@@ -3,10 +3,15 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DatabaseAPI
 {
-    /* Migrations commands: from DatabaseAPI folder
+/* Migrations commands: from DatabaseAPI folder
 dotnet ef --startup-project ../WebAPI/ migrations add concertsImprovement
 dotnet ef --startup-project ../WebAPI/ database update
-     */
+*/
+/* Database migrate to Azure
+pg_dump -Fc -v --host=localhost --port=5430 --username=postgres --dbname=indieWindyDB -f indiewindydb.dump
+pg_restore -v --no-owner --host=indiewindydb.postgres.database.azure.com --port=5432 --username=indie_windy_admin@indiewindydb --dbname=postgres indiewindydb.dump
+ */
+
     public class IndieWindyDbContext:DbContext
     {
         public static string ConnectionString;

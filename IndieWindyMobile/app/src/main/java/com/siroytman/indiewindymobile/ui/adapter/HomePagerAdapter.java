1163e3a9ea4dcf11ff56d12bf90a6b6d381e22a6
Sergey Roytman <SRoytman@croc.ru>
2020-04-01 01:56:16 +0300

    [Front & Back] Main page: news feed #81 Posts latest: init

--- IndieWindyMobile/app/src/main/java/com/siroytman/indiewindymobile/ui/adapter/HomePagerAdapter.java
+++ IndieWindyMobile/app/src/main/java/com/siroytman/indiewindymobile/ui/adapter/HomePagerAdapter.java
@@ -2,6 +2,7 @@ package com.siroytman.indiewindymobile.ui.adapter;
 
 import android.os.Parcelable;
 
+import com.siroytman.indiewindymobile.ui.fragments.home.LatestHomeFragment;
 import com.siroytman.indiewindymobile.ui.fragments.home.SubscriptionHomeFragment;
 
 import androidx.fragment.app.Fragment;
@@ -10,7 +11,7 @@ import androidx.fragment.app.FragmentStatePagerAdapter;
 
 public class HomePagerAdapter extends FragmentStatePagerAdapter {
     public static final String TAG = "HomePagerAdapter";
-    public static int NUM_ITEMS = 1;
+    public static int NUM_ITEMS = 2;
 
     public HomePagerAdapter(FragmentManager fragmentManager) {
         super(fragmentManager);
@@ -28,8 +29,8 @@ public class HomePagerAdapter extends FragmentStatePagerAdapter {
         switch (position) {
             case 0:
                 return SubscriptionHomeFragment.getInstance();
-//            case 1:
-//                return SubscriptionHomeFragment.getInstance();
+            case 1:
+                return LatestHomeFragment.getInstance();
             default:
                 return null;
         }
@@ -41,8 +42,8 @@ public class HomePagerAdapter extends FragmentStatePagerAdapter {
         switch (position) {
             case 0:
                 return "Subscriptions";
-//            case 1:
-//                return "New";
+            case 1:
+                return "Latest";
         }
         return null;
     }
---
+++ IndieWindyMobile/app/src/main/java/com/siroytman/indiewindymobile/ui/fragments/home/LatestHomeFragment.java
@@ -0,0 +1,58 @@
+package com.siroytman.indiewindymobile.ui.fragments.home;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.siroytman.indiewindymobile.R;
+import com.siroytman.indiewindymobile.controller.PostController;
+import com.siroytman.indiewindymobile.model.ArtistPostLink;
+import com.siroytman.indiewindymobile.services.FragmentService;
+import com.siroytman.indiewindymobile.ui.fragments.links.ArtistPostLinkListFragment;
+
+import java.util.ArrayList;
+
+import androidx.fragment.app.Fragment;
+
+public class LatestHomeFragment extends Fragment {
+    public static final String TAG = "LatestHomeFragment";
+    private static LatestHomeFragment instance;
+
+    private LatestHomeFragment() {
+    }
+
+    public static synchronized LatestHomeFragment getInstance() {
+        if (instance == null) {
+            instance = new LatestHomeFragment();
+        }
+        return instance;
+    }
+
+
+    // Store instance variables based on arguments passed
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    // Inflate the view for the fragment based on layout XML
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home_latest, container, false);
+    }
+
+    public void postsFoundViewUpdate(ArrayList<ArtistPostLink> postLinks)
+    {
+        if (postLinks.size() > 0) {
+            Log.d(TAG, "Posts found");
+
+            // Load list fragment
+            ArtistPostLinkListFragment fragment = new ArtistPostLinkListFragment(postLinks);
+            FragmentService.replaceFragment(this, R.id.fragment_home_subscription__container, fragment);
+        } else {
+            FragmentService.clearFragment(this);
+        }
+    }
+}
